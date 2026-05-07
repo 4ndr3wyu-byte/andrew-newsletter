5f9aba953145718c854343010cdad66bb655792b
4ndr3wyu-byte <4ndr3wyu@gmail.com>
2026-05-07 15:15:35 +1000

    MNL-news-sources.py 만들기

---
+++ MNL-news-sources.py
@@ -0,0 +1,73 @@
+MNL_SOURCES = [
+
+    {
+
+        "name": "The Verge",
+
+        "category": "Tech",
+
+        "rss": "https://www.theverge.com/rss/index.xml"
+
+    },
+
+    {
+
+        "name": "TechCrunch",
+
+        "category": "Tech",
+
+        "rss": "https://techcrunch.com/feed/"
+
+    },
+
+    {
+
+        "name": "Ars Technica",
+
+        "category": "Tech",
+
+        "rss": "https://feeds.arstechnica.com/arstechnica/index"
+
+    },
+
+    {
+
+        "name": "Electrek",
+
+        "category": "Tesla/EV",
+
+        "rss": "https://electrek.co/feed/"
+
+    },
+
+    {
+
+        "name": "Teslarati",
+
+        "category": "Tesla/EV",
+
+        "rss": "https://www.teslarati.com/feed/"
+
+    },
+
+    {
+
+        "name": "MacRumors",
+
+        "category": "Apple",
+
+        "rss": "https://www.macrumors.com/macrumors.xml"
+
+    },
+
+    {
+
+        "name": "9to5Mac",
+
+        "category": "Apple",
+
+        "rss": "https://9to5mac.com/feed/"
+
+    }
+
+]

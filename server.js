f475bd6213474a599bbd6e279d41289850a6d9ef
4ndr3wyu-byte <4ndr3wyu@gmail.com>
2026-05-07 15:32:01 +1000

    server.js 업데이트

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 app.get("/MNL-run-news", async function(req, res) {
 
+  try {
+
+    console.log("MNL news bot started");
+
+    res.json({
+
+      ok: true,
+
+      message: "MNL news endpoint works"
+
+    });
+
+  } catch (error) {
+
+    console.error("MNL news error:", error);
+
+    res.status(500).json({
+
+      ok: false,
+
+      error: error.message
+
+    });
+
+  }
+
 });
